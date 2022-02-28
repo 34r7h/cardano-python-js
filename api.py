94590d9904d4f96abebf27638d4ada43bf971dd5
34r7h <2566560+34r7h@users.noreply.github.com>
2022-02-28 15:40:21 +0000

    testing python bridge to js land

---
+++ api.py
@@ -0,0 +1,29 @@
+from pycardano import *
+network = Network.MAINNET
+# psk = PaymentSigningKey.load("payment.skey")
+# Assume there is a stake.skey file sitting in current directory
+# ssk = StakeSigningKey.load("stake.skey")
+
+# pvk = PaymentVerificationKey.from_signing_key(psk)
+# svk = StakeVerificationKey.from_signing_key(ssk)
+
+payment_key_pair = PaymentKeyPair.generate()
+payment_signing_key = payment_key_pair.signing_key
+payment_verification_key = payment_key_pair.verification_key
+
+stake_key_pair = StakeKeyPair.generate()
+stake_signing_key = stake_key_pair.signing_key
+stake_verification_key = stake_key_pair.verification_key
+
+# Save
+payment_signing_key.save("payment.skey")
+payment_verification_key.save("payment.vkey")
+
+# Load
+# payment_signing_key = payment_signing_key.load("payment.skey")
+# payment_verification_key = payment_verification_key.load("payment.vkey")
+
+# Derive an address from payment verification key and stake verification key
+address = Address(payment_verification_key.hash(), stake_verification_key.hash(), network)
+
+print(network, payment_verification_key, stake_signing_key, address)
--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+'use strict';
+
 const api = require('./api.js')
-console.log('connecting python serialization with js / json api', api.api())
+let ret
+let {PythonShell} = require('python-shell')
+PythonShell.runString('x=1+10;print(x)', null, function (err, res) {
+  if (err) throw err;
+  console.log('finished', res);
+  ret = res
+  console.log('connecting python serialization with js / json api', api.api(), ret)
+  return ret
+});
+PythonShell.run('api.py', null, function (err, res) {
+  if (err) throw err;
+  console.log('finished', res);
+});
