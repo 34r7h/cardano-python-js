b85f7614eb15200af2ce7ac0b2abd65bb1237003
34r7h <2566560+34r7h@users.noreply.github.com>
2022-03-05 16:46:32 +0000

    Python serial integration tests, IP to new machine

--- api.js
+++ api.js
@@ -1,2 +1,44 @@
+var fs = require('fs')
+var http = require('http')
+var https = require('https')
+const express = require('express')
+const app = express()
+var cors = require('cors')
+var bodyParser = require('body-parser');
+const { exec } = require("child_process")
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const port = 80
+const portssl = 443
+
 console.log('must arrange endpoints, describe required data structures, etc ')
-exports.api = ()=> 'api js'
+app.get('/', (req, res) => {
+    let obj = '<ul>'
+    // Object.entries(docs).map(z => {
+    //     obj += `<li style=" align-items:center;"><b>${z[0]}</b><br><br>`
+    //     obj += '<div style="overflow-wrap: anywhere;">Example<br><a href="' + encodeURI(z[1].example) + '">' + z[1].example + '</a></div><br>'
+    //     obj += Object.keys(api).includes(z[0].replace('-', '')) ? `Code<br><code style="font: 12px mono; white-space: pre-wrap; background: rgba(0,0,0,.03); padding: 8px">` + api[z[0].replace('-', '')].toString() + `</code><br>` : ''
+    //     obj += '<br></li>'
+    // })
+    return res.send(`
+    <h1>OK start api again</h1>
+    <div>Let's be realistic this time and only expose what's useful and necessary!</div>
+    <ol>
+        <li>QR Codes</li>
+        <li>Minting NFT</li>
+        <li>Listing NFT</li>
+        <li>Selling NFT</li>
+        <li>Create Transaction</li>
+        <li>Submit Transaction</li>
+        <li>Encrypt Message</li>
+        <li>Decrypt Message</li>
+        <li>Sign Message</li>
+        <li>Verify Message</li>
+    </ol>
+    `)
+})
+http.createServer(app).listen(port, () => {
+    console.log(`Example app listening at http://shwifty.io/`)
+})
+exports.api = () => 'api js'
--- api.py
+++ api.py
@@ -1,29 +1,33 @@
 from pycardano import *
 network = Network.MAINNET
-# psk = PaymentSigningKey.load("payment.skey")
+psk = PaymentSigningKey.load("payment.skey")
 # Assume there is a stake.skey file sitting in current directory
-# ssk = StakeSigningKey.load("stake.skey")
+ssk = StakeSigningKey.load("stake.skey")
 
-# pvk = PaymentVerificationKey.from_signing_key(psk)
-# svk = StakeVerificationKey.from_signing_key(ssk)
+pvk = PaymentVerificationKey.from_signing_key(psk)
+svk = StakeVerificationKey.from_signing_key(ssk)
 
-payment_key_pair = PaymentKeyPair.generate()
-payment_signing_key = payment_key_pair.signing_key
-payment_verification_key = payment_key_pair.verification_key
+# payment_key_pair = PaymentKeyPair.generate()
+# payment_signing_key = payment_key_pair.signing_key
+# payment_verification_key = payment_key_pair.verification_key
 
-stake_key_pair = StakeKeyPair.generate()
-stake_signing_key = stake_key_pair.signing_key
-stake_verification_key = stake_key_pair.verification_key
+# stake_key_pair = StakeKeyPair.generate()
+# stake_signing_key = stake_key_pair.signing_key
+# stake_verification_key = stake_key_pair.verification_key
 
 # Save
-payment_signing_key.save("payment.skey")
-payment_verification_key.save("payment.vkey")
+# payment_signing_key.save("payment.skey")
+# payment_verification_key.save("payment.vkey")
+# stake_signing_key.save("stake.skey")
+# stake_verification_key.save("stake.vkey")
 
 # Load
 # payment_signing_key = payment_signing_key.load("payment.skey")
 # payment_verification_key = payment_verification_key.load("payment.vkey")
+# stake_signing_key = payment_signing_key.load("stake.skey")
+# stake_verification_key = payment_verification_key.load("stake.vkey")
 
 # Derive an address from payment verification key and stake verification key
-address = Address(payment_verification_key.hash(), stake_verification_key.hash(), network)
+address = Address(pvk.hash(), svk.hash(), network)
 
-print(network, payment_verification_key, stake_signing_key, address)
+print(address)
