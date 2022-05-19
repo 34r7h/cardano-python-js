63e90927dc4d8b6d23699ecfd018d9bec79576a4
34r7h <2566560+34r7h@users.noreply.github.com>
2022-05-19 16:11:38 +0530

    automating push

--- python/getaddress.py
+++ python/getaddress.py
@@ -2,12 +2,12 @@ from pycardano import Address, Network
 import json
 import sys
 
-args = list(sys.argv)
-print(sys.argv)
-# secret = json.loads(sys.argv)
+args = sys.argv[1:]
+secret = json.loads(args)
+print(secret)
 
-# payment_signing_key = secret
-# payment_verification_key = open("testpayment.vkey", 'r')
+payment_signing_key = secret
+payment_verification_key = open("testpayment.vkey", 'r')
 
 # base_address = Address(payment_part=payment_verification_key.hash(),
 #                        network=Network.MAINNET)
